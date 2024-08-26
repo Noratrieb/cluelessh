6486bb4c812a4ed3f1c461dcd5053c6c26b34942
Noratrieb <48135649+Noratrieb@users.noreply.github.com>
2024-08-26 02:25:46 +0200

    update package name

--- default.nix
+++ default.nix
@@ -1,6 +1,6 @@
 { pkgs ? import <nixpkgs> { }, ... }: pkgs.rustPlatform.buildRustPackage {
   src = pkgs.lib.cleanSource ./.;
-  pname = "ssh";
+  pname = "cluelessh";
   version = "0.1.0";
   cargoLock.lockFile = ./Cargo.lock;
 }
